fae5c06299c038bfa69dfa60844fd38c235ad9d9
darkkrystal <snowpleazz@gmail.com>
2023-09-15 20:06:18 -0700

    Inital Commit

---
+++ september/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div> <h1>Jace has a pooop butt, poop butt </h1>
+    <h1>welcome to react nick </h1>
+    </div>
+  )
+}
+
+export default App
